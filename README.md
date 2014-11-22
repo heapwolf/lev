34a486d64e2b9d62f9c05aa6f13c04284a157efe
Paolo Fragomeni <paolo@async.ly>
2014-11-22 15:38:01 +0100

    version3 rewrite, refactor cli tools

---
+++ README.md
@@ -0,0 +1,7 @@
+# SYNOPSIS
+A simple commandline tool and REPL for leveldb.
+
+# FEATURES
+- REPL with colorized auto-complete and key suggestions
+- REPL automatically saves and reloads REPL history
+
--- index.js
+++ index.js
@@ -4,12 +4,27 @@ var createREPL = require('./lib/repl');
 var getDB = require('./lib/db');
 var locate = require('./lib/location');
 var cache = require('./lib/cache');
+var cli = require('./lib/cli');
 
 module.exports = function(args) {
 
   locate(args);
 
   var db = getDB(args);
+
+  var cliCommands = [
+    'keys', 'values', 'get', 
+    'put', 'del', 'createReadStream', 'batch'
+  ];
+  
+  var cliMode = Object.keys(args).some(function(cmd) {
+    return cliCommands.indexOf(cmd) > -1;
+  });
+ 
+  if (cliMode) {
+    return cli(db, args);
+  }
+
   var repl = createREPL(db, args, cache);
 
   history(repl, args);
